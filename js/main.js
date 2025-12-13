6ff646eb68791c7524240006e676aa0e20abae6d
Jay Rudel <jayrudel@u.boisestate.edu>
2025-12-12 21:00:35 -0700

    Preventing top 3 deletions

--- js/main.js
+++ js/main.js
@@ -19,19 +19,19 @@ function loadItems() {
 
             // Checking each item and then adding them to the table.
             data.forEach(item => {
-                // if(item.id == "Jay" || item.id == "Dee" || item.id == "Enid"){
-                //     const row = `<tr>
-                //                     <td>${item.id}</td>
-                //                     <td>${item.contactInfo}</td>
-                //                     <td>${item.speciality}</td>
-                //                     <td>${item.helped}</td>
-                //                     <td>${item.rate}</td>
-                //                     <td>Unable to Delete</td>
-                //                 </tr>`;
-                //     // Cant delete These three.
-                // table.innerHTML += row;
-                // }
-                // else{
+                if(item.id === "Jay" || item.id === "Dee" || item.id === "Enid"){
+                    const row = `<tr>
+                                    <td>${item.id}</td>
+                                    <td>${item.contactInfo}</td>
+                                    <td>${item.speciality}</td>
+                                    <td>${item.helped}</td>
+                                    <td>${item.rate}</td>
+                                    <td>Unable to Delete</td>
+                                </tr>`;
+                    // Cant delete These three.
+                table.innerHTML += row;
+                }
+                else{
                     const row = `<tr>
                                     <td>${item.id}</td>
                                     <td>${item.contactInfo}</td>
@@ -42,7 +42,7 @@ function loadItems() {
                                 </tr>`;
 
                 table.innerHTML += row;
-                // }
+                }
                 
             });
         }); // Cleaning all the closing brackets.
